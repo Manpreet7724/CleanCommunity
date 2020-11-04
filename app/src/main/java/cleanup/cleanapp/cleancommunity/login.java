c6a3850dd8a3e607dc5c90f884e9c6226ea02e9d
KevinDelgado <kevindaniel7x@gmail.com>
2020-11-04 12:23:17 -0500

    Signup Class connects and sends info to database

--- app/src/main/java/cleanup/cleanapp/cleancommunity/login.java
+++ app/src/main/java/cleanup/cleanapp/cleancommunity/login.java
@@ -1,15 +1,49 @@
 package cleanup.cleanapp.cleancommunity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class login  extends AppCompatActivity
 {
+    String urlAddress = "http://apollo.humber.ca/~n01323567/login.php";
+    String nameTxt,emailTxt,phoneTxt,passwordTxt,conPasswordTxt;
+    EditText editName, editEmail, editPhone, editPassword, editConPass;
+    private static final String TAG = "Error found: ";
+    URL url = new URL(urlAddress);
+
+    public login() throws MalformedURLException {
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) // tells user the activy is created
     {
@@ -18,23 +52,66 @@ public class login  extends AppCompatActivity
     }
     public void templogin(View view)
     {
-        EditText text = findViewById(R.id.editPasswordLog);
-        String pass = text.getText().toString().replaceAll("\\s+", "");
-        text = findViewById(R.id.editTusername);
-        String email = text.getText().toString().replaceAll("\\s+", "");
-        int val =storeinfo.templogin(email,pass);
-        if (val!=1)
-        {
-            TextView wongtext = findViewById(R.id.wongText);
-            wongtext.setVisibility(View.VISIBLE);
-        }
-        else
-        {
-            final Intent intent = new Intent(this, getstarted.class);
-            startActivity(intent);
-        }
+        int x;
+        editEmail = (EditText) findViewById(R.id.editTusername);
+        editPassword = (EditText) findViewById(R.id.editPasswordLog);
+
+        emailTxt = editEmail.getText().toString().trim();
+        passwordTxt = editPassword.getText().toString().trim();
+
+       ReceiveData(emailTxt, passwordTxt);
+
 
     }
+
+   private void  ReceiveData(final String email,final String password) {
+       class SendPostReqAsyncTask extends AsyncTask<String, Void, String> {
+           @Override
+           protected String doInBackground(String... params) {
+               InputStream is = null;
+
+               List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+               nameValuePairs.add(new BasicNameValuePair("Email", emailTxt));
+               nameValuePairs.add(new BasicNameValuePair("Password", passwordTxt));
+               String result = null;
+               try {
+                   HttpClient httpClient = new DefaultHttpClient();
+                   HttpPost httpPost = new HttpPost(urlAddress);
+                   httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, HTTP.UTF_8));
+                   HttpResponse httpResponse = httpClient.execute(httpPost);
+                   HttpEntity httpEntity = httpResponse.getEntity();
+
+                   is = httpEntity.getContent();
+
+                   BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"), 8);
+                   StringBuilder sb = new StringBuilder();
+                   String line = null;
+                   while ((line = reader.readLine()) != null) {
+                       sb.append(line + "\n");
+                   }
+                   result = sb.toString();
+               } catch (IOException e) {
+                   Log.e("log_tag", "Error in http connection", e);
+               }
+               return result;
+           }
+
+           @Override
+           protected void onPostExecute(String result) {
+               String s = result.trim();
+               if (s.equalsIgnoreCase("success")) {
+                   final Intent intent = new Intent(login.this, getstarted.class);
+                   startActivity(intent);
+                   finish();
+
+               } else {
+                   Toast.makeText(getApplicationContext(), "Invalid Email or Password", Toast.LENGTH_LONG).show();
+               }
+           }
+       }
+       new SendPostReqAsyncTask().execute(email, password);
+   }
+
     @Override
     protected void onStop() // tells user the activy was stoped
     {
--- app/src/main/java/cleanup/cleanapp/cleancommunity/signUp.java
+++ app/src/main/java/cleanup/cleanapp/cleancommunity/signUp.java
@@ -1,16 +1,64 @@
 package cleanup.cleanapp.cleancommunity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import org.apache.http.*;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import org.apache.http.*;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+
 public class signUp extends AppCompatActivity
 {
+    String urlAddress = "http://apollo.humber.ca/~n01323567/signup.php";
+    String nameTxt,emailTxt,phoneTxt,passwordTxt,conPasswordTxt;
+    EditText editName, editEmail, editPhone, editPassword, editConPass;
+    private static final String TAG = "Error found: ";
+    URL url = new URL(urlAddress);
 
+    public signUp() throws MalformedURLException {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) // tells user the activy is created
@@ -19,29 +67,64 @@ public class signUp extends AppCompatActivity
         setContentView(R.layout.signup);
     }
 
+    @SuppressLint("LongLogTag")
     public void adddata(View view)
     {
         int x;
-        EditText text = findViewById(R.id.editPersonName);
-        String name = text.getText().toString();
-        text = findViewById(R.id.editEmail);
-        String email = text.getText().toString().replaceAll("\\s+", "");
-        text = findViewById(R.id.editPhone);
-        String phone = text.getText().toString().replaceAll("\\s+", "");
-        text = findViewById(R.id.editPassword);
-        String pass = text.getText().toString().replaceAll("\\s+", "");
-        text = findViewById(R.id.editConPassword);
-        String conPass = text.getText().toString().replaceAll("\\s+", "");
+        editName = (EditText) findViewById(R.id.editPersonName);
+        editEmail = (EditText) findViewById(R.id.editEmail);
+        editPhone = (EditText) findViewById(R.id.editPhone);
+        editPassword = (EditText) findViewById(R.id.editPassword);
+        editConPass = (EditText) findViewById(R.id.editConPassword);
 
-        x = storeinfo.adddata(name,email,phone,pass,conPass);
-        if(x == 1)
-        {
+        nameTxt = editName.getText().toString().trim();
+        emailTxt = editEmail.getText().toString().trim();
+        phoneTxt = editPhone.getText().toString().trim();
+        passwordTxt = editPassword.getText().toString().trim();
+        conPasswordTxt = editConPass.getText().toString().trim();
+
+        if(passwordTxt.equals(conPasswordTxt)){
+            InsertData(nameTxt, emailTxt, phoneTxt, passwordTxt);
             final Intent intent = new Intent(this, getstarted.class);
             startActivity(intent);
+        }else{
+            Toast.makeText(signUp.this, "Passwords do not match", Toast.LENGTH_LONG).show();
         }
 
-
     }
+
+    public void InsertData(final String name, final String email, final String phone, final String password){
+        class SendPostReqAsyncTask extends AsyncTask<String, Void, String> {
+            @Override
+            protected String doInBackground(String... params) {
+
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("Name", nameTxt));
+                nameValuePairs.add(new BasicNameValuePair("Email", emailTxt));
+                nameValuePairs.add(new BasicNameValuePair("Phone", phoneTxt));
+                nameValuePairs.add(new BasicNameValuePair("Password", passwordTxt));
+                try {
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpPost httpPost = new HttpPost(urlAddress);
+                    httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                    HttpResponse httpResponse = httpClient.execute(httpPost);
+                    HttpEntity httpEntity = httpResponse.getEntity();
+                }  catch (IOException e) {
+                    Log.e("log_tag", "Error in http connection", e);
+                }
+
+                return "Data Inserted Successfully";
+            }
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+                Toast.makeText(signUp.this, "Sign Up Successful", Toast.LENGTH_LONG).show();
+            }
+        }
+        SendPostReqAsyncTask sendPostReqAsyncTask = new SendPostReqAsyncTask();
+        sendPostReqAsyncTask.execute(name, email, phone, password);
+    }
+
 
     protected void onStart() // tells user the activy is started
     {
