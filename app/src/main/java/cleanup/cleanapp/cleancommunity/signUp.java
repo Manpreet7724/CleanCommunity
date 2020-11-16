8071236e3f62c2f978e3733574778519ceab624b
KevinDelgadosHaven <71301117+KevinDelgadosHaven@users.noreply.github.com>
2020-11-16 05:33:47 -0500

    Commit 45 - Added Password confirmation in signup

--- app/src/main/java/cleanup/cleanapp/cleancommunity/signUp.java
+++ app/src/main/java/cleanup/cleanapp/cleancommunity/signUp.java
@@ -16,8 +16,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -60,8 +63,14 @@ public class signUp extends AppCompatActivity
         editPassword = findViewById(R.id.editPassword);
         editConPass = findViewById(R.id.editConPassword);
 
+        passwordTxt = editPassword.getText().toString().trim();
+        conPasswordTxt = editConPass.getText().toString().trim();
 
-        registerUserdata();
+        if(passwordTxt.equals(conPasswordTxt)){
+            registerUserdata();
+        }else{
+            Toast.makeText(signUp.this, "Passwords do not Match", Toast.LENGTH_LONG).show();
+        }
 
     }
 
